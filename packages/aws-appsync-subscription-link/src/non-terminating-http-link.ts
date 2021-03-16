9e43152eba5e6ac7860c33dd0b54939ffaa1897f
Marco <mturi@topcon.com>
2021-03-16 18:43:51 +0100

    chore: implement PR for reduce bundle size

--- packages/aws-appsync-subscription-link/src/non-terminating-http-link.ts
+++ packages/aws-appsync-subscription-link/src/non-terminating-http-link.ts
@@ -2,13 +2,13 @@
  * Copyright 2017-2018 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
-import { createHttpLink, HttpOptions } from '@apollo/client';
+import { createHttpLink, HttpOptions } from '@apollo/client/core';
 import { NonTerminatingLink } from './non-terminating-link';
 
 export class NonTerminatingHttpLink extends NonTerminatingLink {
-    constructor(contextKey: string, options: HttpOptions) {
-        const link = createHttpLink(options);
+	constructor(contextKey: string, options: HttpOptions) {
+		const link = createHttpLink(options);
 
-        super(contextKey, { link });
-    }
+		super(contextKey, { link });
+	}
 }
